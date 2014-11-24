452b6f88c04abfcbb9d74a5f867fb061cecea3b6
Zi Chen <zchen@mdsol>
2014-11-24 18:25:40 -0500

    more progress

--- hw2/main.js
+++ hw2/main.js
@@ -5,11 +5,15 @@ app.controller('Hw2Controller', function($scope) {
   var self = this;
   
   self.timelineEvents = [
-	{"description" : "Getting started Programming",
-	 "title" : "programming"
+	{
+		"title" : "High School",
+		"description" : "Graduated from High School",
+		"date" : (new Date(2002, 6, 1)).toJSON()
 	},
-	{"description" : "Getting started Programming",
-		"title" : "programming"
+	{
+		"title" : "College",
+		"description" : "Graduated from College",
+		"date" : (new Date(2006, 5, 30)).toJSON()
 	}  
   ];
   
