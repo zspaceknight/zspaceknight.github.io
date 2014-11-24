4e623f0c8d43367b949f2a219d860efa3758ac6d
ZC <zspaceknight@gmail.com>
2014-11-24 17:56:57 -0500

    hw2

---
+++ hw2/main.js
@@ -0,0 +1,17 @@
+var app = angular.module('Hw2App', []);
+
+app.controller('Hw2Controller', function($scope) {
+  
+  var self = this;
+  
+  self.timelineEvents = [
+	{"description" : "Getting started Programming",
+	 "title" : "programming"
+	},
+	{"description" : "Getting started Programming",
+		"title" : "programming"
+	}  
+  ];
+  
+});
+
